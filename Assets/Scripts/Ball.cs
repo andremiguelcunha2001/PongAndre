9052b8cb7cdeb1da22b4fa361aa4ae9a23d262ee
andremiguelcunha2001 <40200331@esmad.ipp.pt>
2021-03-22 22:06:15 +0000

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+   float Velocidade = 5f;
+
+    float tempo = 0f;
+    float lancarbola = 2f;
+    bool estadobola = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo = tempo + Time.deltaTime;
+
+        if (estadobola == true)
+        {
+            if (tempo > lancarbola)
+            {
+                GetComponent<Rigidbody2D>().velocity = Velocidade * Random.insideUnitSphere;
+                estadobola = false;
+            }
+
+        }
+    }
+  
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        /**
+         * se tecla para cima premida
+         * então subir
+         * senão se tecla para baixo premida
+         * então descer
+         */
+
+
+
+        if (Input.GetKey(upKey))
+        {
+            // tarnsform.position += velocidade * vector3.up 
+
+            transform.position =
+                transform.position + velocidade * Vector3.up * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.position =
+                transform.position + velocidade * Vector3.down * Time.deltaTime;
+            //Desce
+        }
+
+        float cameraHeight = Camera.main.orthographicSize;
+        float halfPaddleSize = 0.5f;
+
+        if (transform.position.y >= cameraHeight - halfPaddleSize)
+        {
+            // Na prática o códio abaixo faz isto: transform.position.y = 3f - 0.5f
+
+            Vector3 positionAux = transform.position;
+            positionAux.y = cameraHeight - halfPaddleSize;
+            transform.position = positionAux;
+        }
+
+
+        if (transform.position.y <= -cameraHeight + halfPaddleSize)
+        {
+            // Na prática o códio abaixo faz isto: transform.position.y = 3f - 0.5f
+
+            Vector3 positionAux = transform.position;
+            positionAux.y = -cameraHeight + halfPaddleSize;
+            transform.position = positionAux;
+
+        }
+
+    }
+}
